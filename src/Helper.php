2c65e9056f730ac235185bcc8b5eccc68e3d1fe6
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-05-24 13:44:20 +0800

    fix php format.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- src/Helper.php
+++ src/Helper.php
@@ -4,25 +4,27 @@ namespace Howtomakeaturn\Pay2goInvoice;
 
 class Helper
 {
-
-    static function addpadding($string, $blocksize = 32) {
+    public static function addpadding($string, $blocksize = 32)
+    {
         $len = strlen($string);
         $pad = $blocksize - ($len % $blocksize);
         $string .= str_repeat(chr($pad), $pad);
+
         return $string;
     }
 
-    static function curl_work($url = "", $parameter = "") {
+    public static function curl_work($url = '', $parameter = '')
+    {
         $curl_options = array(
         CURLOPT_URL => $url,
         CURLOPT_HEADER => false,
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_USERAGENT => "Google Bot",
+        CURLOPT_USERAGENT => 'Google Bot',
         CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_SSL_VERIFYPEER => FALSE,
-        CURLOPT_SSL_VERIFYHOST => FALSE,
-        CURLOPT_POST => "1",
-        CURLOPT_POSTFIELDS => $parameter
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_POST => '1',
+        CURLOPT_POSTFIELDS => $parameter,
         );
         $ch = curl_init();
         curl_setopt_array($ch, $curl_options);
@@ -31,13 +33,13 @@ class Helper
         $curl_error = curl_errno($ch);
         curl_close($ch);
         $return_info = array(
-            "url" => $url,
-            "sent_parameter" => $parameter,
-            "http_status" => $retcode,
-            "curl_error_no" => $curl_error,
-            "web_info" => $result
+            'url' => $url,
+            'sent_parameter' => $parameter,
+            'http_status' => $retcode,
+            'curl_error_no' => $curl_error,
+            'web_info' => $result,
         );
+
         return $return_info;
     }
-
 }
